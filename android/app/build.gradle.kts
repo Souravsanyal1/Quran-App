daccb9ba25ca01e1cbbd2a9d62577b5c0ccb7e93
Souravsanyal1 <souravsanyal1@users.noreply.github.com>
2026-08-04 16:52:46 +0600

    fix(android): set JDK 17, buildToolsVersion 36.0.0 and compileSdk 35 for APK build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 
 android {
     namespace = "com.quranapp.quran_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
+    buildToolsVersion = "36.0.0"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -25,20 +26,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.quranapp.quran_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
